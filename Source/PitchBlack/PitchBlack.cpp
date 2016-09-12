464f3b306312772ea5e6a71d5dc9732b31de7637
Eugene Shcherbakov <evgeniyshb@gmail.com>
2016-09-12 23:41:46 +0300

    Unreal Engine 4 empty project

---
+++ Source/PitchBlack/PitchBlack.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PitchBlack.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PitchBlack, "PitchBlack" );
